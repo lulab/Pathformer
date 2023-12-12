ddc7bde57e6a540b4354d474f6dab46929ab1150
xfliu1995 <774327013@qq.com>
2023-12-12 10:40:41 +0800

    Update model.py

--- comparison_methods/early_integration_method/model.py
+++ comparison_methods/early_integration_method/model.py
@@ -12,16 +12,16 @@ from sklearn.neural_network import MLPClassifier
 from lightgbm.sklearn import LGBMClassifier
 import xgboost as xgb
 from sklearn.utils.class_weight import compute_sample_weight
-from sklearn.metrics import roc_curve, roc_auc_score, auc, precision_recall_curve, average_precision_score, f1_score, accuracy_score
+from sklearn.metrics import roc_curve, roc_auc_score, auc, precision_recall_curve, average_precision_score, f1_score, \
+    accuracy_score
 from scipy.stats import rankdata
 import argparse, sys, os, errno
 import logging
 import warnings
 import joblib
 import random
+
 warnings.filterwarnings("ignore")
-
-
 
 
 def plot_roc(prob, label):
@@ -31,14 +31,16 @@ def plot_roc(prob, label):
     # print (fpr, tpr,ppv)
     return roc_auc, fpr, tpr, precision, recall
 
+
 def get_roc(y, pro):
-    label=np.array(y)
-    pre_label = (pro>0.5).astype(int)
-    auc=roc_auc_score(label, np.array(pro))
+    label = np.array(y)
+    pre_label = (pro > 0.5).astype(int)
+    auc = roc_auc_score(label, np.array(pro))
     acc = accuracy_score(label, pre_label)
     f1_macro = f1_score(label, pre_label, average='macro')
     f1_weighted = f1_score(label, pre_label, average='weighted')
-    return acc, auc,f1_weighted, f1_macro
+    return acc, auc, f1_weighted, f1_macro
+
 
 def plot_roc_multi(prob, label):
     pre_label = prob.argmax(axis=1)
@@ -64,15 +66,16 @@ def clf_select(name, pds):
         tree_para = {'C': [0.01, 0.1, 1, 10, 100]}
         clf = GridSearchCV(SVC(kernel='rbf', probability=True), tree_para, cv=pds, n_jobs=5, scoring='f1_macro')
     elif name == 'RF':
-        clf = RandomForestClassifier(n_estimators=50, max_depth=1000,ccp_alpha=0.1)
+        clf = RandomForestClassifier(n_estimators=50, max_depth=1000, ccp_alpha=0.1)
     elif name == 'RF_cv':
-        tree_para = {'n_estimators': [10,50, 100, 200, 500], 'max_depth': [10, 50,100,200, 500]}
+        tree_para = {'n_estimators': [10, 50, 100, 200, 500], 'max_depth': [10, 50, 100, 200, 500]}
         clf = GridSearchCV(RandomForestClassifier(), tree_para, cv=pds, n_jobs=5, scoring='f1_macro')
     elif name == 'LR':
         clf = LogisticRegression(penalty='l2', solver='liblinear', C=1)
     elif name == 'LR_cv':
-        tree_para = {'C': [ 0.001, 0.1, 1, 10, 100]}
-        clf = GridSearchCV(LogisticRegression(penalty='l2', solver='liblinear'), tree_para, cv=pds,n_jobs=5, scoring='f1_macro')
+        tree_para = {'C': [0.001, 0.1, 1, 10, 100]}
+        clf = GridSearchCV(LogisticRegression(penalty='l2', solver='liblinear'), tree_para, cv=pds, n_jobs=5,
+                           scoring='f1_macro')
     elif name == 'KNN':
         clf = KNeighborsClassifier(n_neighbors=10, weights='distance', leaf_size=10)
     elif name == 'KNN_cv':
@@ -81,9 +84,10 @@ def clf_select(name, pds):
     elif name == 'NN':
         clf = MLPClassifier(hidden_layer_sizes=(100), max_iter=200)
     elif name == 'LGBoost':
-        clf = LGBMClassifier( num_leaves=5, n_estimators=100)
+        clf = LGBMClassifier(num_leaves=5, n_estimators=100)
     elif name == 'LGBoost_cv':
-        tree_para = {'max_depth': [5,10,50,100,500,1000], 'n_estimators': [100,500,1000],'num_leaves':[20,30,50,100]}
+        tree_para = {'max_depth': [5, 10, 50, 100, 500, 1000], 'n_estimators': [100, 500, 1000],
+                     'num_leaves': [20, 30, 50, 100]}
         clf = GridSearchCV(LGBMClassifier(learning_rate=0.1), tree_para, cv=pds, n_jobs=5, scoring='f1_macro')
     elif name == 'XGBoost':
         clf = xgb.XGBClassifier(learning_rate=0.5, n_estimators=500, max_depth=50, min_child_weight=3,
@@ -96,7 +100,8 @@ def clf_select(name, pds):
         tree_para = {'max_depth': [10, 50, 100, 200, 500], 'n_estimators': [50, 100, 200, 500]}
         clf = GridSearchCV(xgb.XGBClassifier(learning_rate=0.5, min_child_weight=3, gamma=3, subsample=0.7,
                                              objective='binary:logistic',
-                                             scale_pos_weight=1, nthread=5), tree_para, cv=pds,n_jobs=5, scoring='f1_macro')
+                                             scale_pos_weight=1, nthread=5), tree_para, cv=pds, n_jobs=5,
+                           scoring='f1_macro')
     return clf
 
 
@@ -110,17 +115,18 @@ def clf_select_multi(name, pds):
         clf = SVC(kernel='rbf', probability=True, C=1)
     elif name == 'SVM_cv':
         tree_para = {'C': [0.01, 0.1, 1, 10, 100]}
-        clf = GridSearchCV(SVC(kernel='rbf', probability=True), tree_para, cv=pds, n_jobs=5,scoring='f1_macro')
+        clf = GridSearchCV(SVC(kernel='rbf', probability=True), tree_para, cv=pds, n_jobs=5, scoring='f1_macro')
     elif name == 'RF':
         clf = RandomForestClassifier(n_estimators=100, max_depth=100)
     elif name == 'RF_cv':
-        tree_para = {'n_estimators': [10,50, 100, 200, 500], 'max_depth': [10, 50,100,200, 500]}
+        tree_para = {'n_estimators': [10, 50, 100, 200, 500], 'max_depth': [10, 50, 100, 200, 500]}
         clf = GridSearchCV(RandomForestClassifier(), tree_para, cv=pds, n_jobs=5, scoring='f1_macro')
     elif name == 'LR':
         clf = LogisticRegression(penalty='l2', solver='liblinear', C=1)
     elif name == 'LR_cv':
-        tree_para = {'C': [ 0.001, 0.1, 1, 10, 100]}
-        clf = GridSearchCV(LogisticRegression(penalty='l2', solver='liblinear'), tree_para, cv=pds,n_jobs=5, scoring='f1_macro')
+        tree_para = {'C': [0.001, 0.1, 1, 10, 100]}
+        clf = GridSearchCV(LogisticRegression(penalty='l2', solver='liblinear'), tree_para, cv=pds, n_jobs=5,
+                           scoring='f1_macro')
     elif name == 'KNN':
         clf = KNeighborsClassifier(n_neighbors=10)
     elif name == 'KNN_cv':
@@ -129,9 +135,10 @@ def clf_select_multi(name, pds):
     elif name == 'NN':
         clf = MLPClassifier(hidden_layer_sizes=(100), max_iter=200)
     elif name == 'LGBoost':
-        clf = LGBMClassifier( num_leaves=5, n_estimators=100)
+        clf = LGBMClassifier(num_leaves=5, n_estimators=100)
     elif name == 'LGBoost_cv':
-        tree_para = {'max_depth': [5,10,50,100,500,1000], 'n_estimators': [100,500,1000],'num_leaves':[20,30,50,100]}
+        tree_para = {'max_depth': [5, 10, 50, 100, 500, 1000], 'n_estimators': [100, 500, 1000],
+                     'num_leaves': [20, 30, 50, 100]}
         clf = GridSearchCV(LGBMClassifier(learning_rate=0.1), tree_para, cv=pds, n_jobs=5, scoring='f1_macro')
     elif name == 'XGBoost':
         clf = xgb.XGBClassifier(learning_rate=0.5, n_estimators=500, max_depth=50, min_child_weight=3,
@@ -155,95 +162,92 @@ def main_model(data_path, label_path, feature_name, dataset, method, num, save_p
     data = data.fillna(0)
 
     label = pd.read_csv(label_path, sep='\t')
-    sample_discovery = label.loc[label['dataset_' + str(dataset)] != 'test', 'sample_id']
-    sample_validation = label.loc[label['dataset_' + str(dataset)] == 'test', 'sample_id']
+    sample_train = label.loc[label['dataset_' + str(dataset)] == 'train', 'sample_id']
+    sample_test = label.loc[label['dataset_' + str(dataset)] == 'test', 'sample_id']
     label = label.set_index('sample_id')
 
-    y_discovery = np.array(label.loc[sample_discovery, 'y'])
-    y_discovery = y_discovery.astype('int')
-    y_validation = np.array(label.loc[sample_validation, 'y'])
-    y_validation = y_validation.astype('int')
+    y_train = np.array(label.loc[sample_train, 'y'])
+    y_train = y_train.astype('int')
+    y_test = np.array(label.loc[sample_test, 'y'])
+    y_test = y_test.astype('int')
 
-    X_discovery = np.array(data.loc[:, sample_discovery]).T
-    X_validation = np.array(data.loc[:, sample_validation]).T
+    X_train = np.array(data.loc[:, sample_train]).T
+    X_test = np.array(data.loc[:, sample_test]).T
 
-    result_predict_discovery = pd.DataFrame(columns={'sample_id', 'label'})
-    result_predict_discovery['sample_id'] = sample_discovery
-    result_predict_discovery['label'] = list(label.loc[sample_discovery, 'y'])
-    result_predict_validation = pd.DataFrame(columns={'sample_id', 'label'})
-    result_predict_validation['sample_id'] = sample_validation
-    result_predict_validation['label'] = list(label.loc[sample_validation, 'y'])
-    result_auc = pd.DataFrame(columns={'AUC_discovery', 'AUC_validation'})
+    result_predict_train = pd.DataFrame(columns={'sample_id', 'label'})
+    result_predict_train['sample_id'] = sample_train
+    result_predict_train['label'] = list(label.loc[sample_train, 'y'])
+    result_predict_test = pd.DataFrame(columns={'sample_id', 'label'})
+    result_predict_test['sample_id'] = sample_test
+    result_predict_test['label'] = list(label.loc[sample_test, 'y'])
+    result_auc = pd.DataFrame(columns={'AUC_train', 'AUC_test'})
     pds = 5
     for i in range(num):
-        sample_weight_ = compute_sample_weight(class_weight='balanced', y=y_discovery)
+        sample_weight_ = compute_sample_weight(class_weight='balanced', y=y_train)
         if len(set(label['y'])) > 2:
             clf = clf_select_multi(method, pds)
         else:
             clf = clf_select(method, pds)
         if (method == 'KNN') | (method == 'KNN_cv') | (method == 'NN'):
-            clf.fit(X_discovery, y_discovery)
+            clf.fit(X_train, y_train)
         else:
-            clf.fit(X_discovery, y_discovery, sample_weight_)
+            clf.fit(X_train, y_train, sample_weight_)
         if len(set(label['y'])) > 2:
-            predict_discovery = clf.predict_proba(X_discovery)
-            predict_validation = clf.predict_proba(X_validation)
+            predict_train = clf.predict_proba(X_train)
+            predict_test = clf.predict_proba(X_test)
 
-            y_discovery_index = list(set(y_discovery))
-            y_discovery_index.sort()
-            y_validation_index = list(set(y_validation))
-            y_validation_index.sort()
+            y_train_index = list(set(y_train))
+            y_train_index.sort()
+            y_test_index = list(set(y_test))
+            y_test_index.sort()
 
-            predict_discovery_ = predict_discovery[:, y_discovery_index] / predict_discovery[:, y_discovery_index].sum(
-                axis=1, keepdims=1)
-            predict_validation_ = predict_validation[:, y_validation_index] / predict_validation[:,
-                                                                              y_validation_index].sum(axis=1,
-                                                                                                      keepdims=1)
-            predict_discovery_[np.isnan(predict_discovery_)] = 1 / predict_discovery_.shape[1]
-            predict_validation_[np.isnan(predict_validation_)] = 1 / predict_validation_.shape[1]
-            acc_discovery, auc_weighted_ovr_discovery, auc_weighted_ovo_discovery, auc_macro_ovr_discovery, auc_macro_ovo_discovery, f1_weighted_discovery, f1_macro_discovery = plot_roc_multi(
-                predict_discovery_, rankdata(y_discovery, method='dense') - 1)
-            acc_validation, auc_weighted_ovr_validation, auc_weighted_ovo_validation, auc_macro_ovr_validation, auc_macro_ovo_validation, f1_weighted_validation, f1_macro_validation = plot_roc_multi(
-                predict_validation_, rankdata(y_validation, method='dense') - 1)
+            predict_train_ = predict_train[:, y_train_index] / predict_train[:, y_train_index].sum(axis=1, keepdims=1)
+            predict_test_ = predict_test[:, y_test_index] / predict_test[:,y_test_index].sum(axis=1,keepdims=1)
+            predict_train_[np.isnan(predict_train_)] = 1 / predict_train_.shape[1]
+            predict_test_[np.isnan(predict_test_)] = 1 / predict_test_.shape[1]
+            acc_train, auc_weighted_ovr_train, auc_weighted_ovo_train, auc_macro_ovr_train, auc_macro_ovo_train, f1_weighted_train, f1_macro_train = plot_roc_multi(
+                predict_train_, rankdata(y_train, method='dense') - 1)
+            acc_test, auc_weighted_ovr_test, auc_weighted_ovo_test, auc_macro_ovr_test, auc_macro_ovo_test, f1_weighted_test, f1_macro_test = plot_roc_multi(
+                predict_test_, rankdata(y_test, method='dense') - 1)
 
-            for y_num in list(set(y_discovery)):
-                result_predict_discovery['num_' + str(y_num) + '_' + str(i)] = predict_discovery[:, y_num]
-                result_predict_validation['num_' + str(y_num) + '_' + str(i)] = predict_validation[:, y_num]
+            for y_num in list(set(y_train)):
+                result_predict_train['num_' + str(y_num) + '_' + str(i)] = predict_train[:, y_num]
+                result_predict_test['num_' + str(y_num) + '_' + str(i)] = predict_test[:, y_num]
 
-            result_auc.loc[i, 'acc_discovery'] = acc_discovery
-            result_auc.loc[i, 'auc_weighted_ovr_discovery'] = auc_weighted_ovr_discovery
-            result_auc.loc[i, 'auc_weighted_ovo_discovery'] = auc_weighted_ovo_discovery
-            result_auc.loc[i, 'auc_macro_ovr_discovery'] = auc_macro_ovr_discovery
-            result_auc.loc[i, 'auc_macro_ovo_discovery'] = auc_macro_ovo_discovery
-            result_auc.loc[i, 'f1_weighted_discovery'] = f1_weighted_discovery
-            result_auc.loc[i, 'f1_macro_discovery'] = f1_macro_discovery
-            result_auc.loc[i, 'acc_validation'] = acc_validation
-            result_auc.loc[i, 'auc_weighted_ovr_validation'] = auc_weighted_ovr_validation
-            result_auc.loc[i, 'auc_weighted_ovo_validation'] = auc_weighted_ovo_validation
-            result_auc.loc[i, 'auc_macro_ovr_validation'] = auc_macro_ovr_validation
-            result_auc.loc[i, 'auc_macro_ovo_validation'] = auc_macro_ovo_validation
-            result_auc.loc[i, 'f1_weighted_validation'] = f1_weighted_validation
-            result_auc.loc[i, 'f1_macro_validation'] = f1_macro_validation
+            result_auc.loc[i, 'acc_train'] = acc_train
+            result_auc.loc[i, 'auc_weighted_ovr_train'] = auc_weighted_ovr_train
+            result_auc.loc[i, 'auc_weighted_ovo_train'] = auc_weighted_ovo_train
+            result_auc.loc[i, 'auc_macro_ovr_train'] = auc_macro_ovr_train
+            result_auc.loc[i, 'auc_macro_ovo_train'] = auc_macro_ovo_train
+            result_auc.loc[i, 'f1_weighted_train'] = f1_weighted_train
+            result_auc.loc[i, 'f1_macro_train'] = f1_macro_train
+            result_auc.loc[i, 'acc_test'] = acc_test
+            result_auc.loc[i, 'auc_weighted_ovr_test'] = auc_weighted_ovr_test
+            result_auc.loc[i, 'auc_weighted_ovo_test'] = auc_weighted_ovo_test
+            result_auc.loc[i, 'auc_macro_ovr_test'] = auc_macro_ovr_test
+            result_auc.loc[i, 'auc_macro_ovo_test'] = auc_macro_ovo_test
+            result_auc.loc[i, 'f1_weighted_test'] = f1_weighted_test
+            result_auc.loc[i, 'f1_macro_test'] = f1_macro_test
         else:
-            predict_discovery = clf.predict_proba(X_discovery)
-            predict_validation = clf.predict_proba(X_validation)
-            
-            acc_discovery, auc_discovery, f1_weighted_discovery, f1_macro_discovery = get_roc(y_discovery, predict_discovery[:, 1])
-            acc_validation, auc_validation, f1_weighted_validation, f1_macro_validation = get_roc(y_validation, predict_validation[:, 1])
-            result_predict_discovery['num_' + str(i)] = predict_discovery[:, 1]
-            result_predict_validation['num_' + str(i)] = predict_validation[:, 1]
-            result_auc.loc[i, 'acc_discovery'] = acc_discovery
-            result_auc.loc[i, 'auc_discovery'] = auc_discovery
-            result_auc.loc[i, 'f1_weighted_discovery'] = f1_weighted_discovery
-            result_auc.loc[i, 'f1_macro_discovery'] = f1_macro_discovery
-            result_auc.loc[i, 'acc_validation'] = acc_validation
-            result_auc.loc[i, 'auc_validation'] = auc_validation
-            result_auc.loc[i, 'f1_weighted_validation'] = f1_weighted_validation
-            result_auc.loc[i, 'f1_macro_validation'] = f1_macro_validation
+            predict_train = clf.predict_proba(X_train)
+            predict_test = clf.predict_proba(X_test)
 
-    result_predict_discovery.to_csv(save_path  + '/' + str(method.split('_cv')[0]) + '/result_newnew_predict_discovery_' + feature_name + '.txt', sep='\t', index=False)
-    result_predict_validation.to_csv(save_path + '/' + str(method.split('_cv')[0]) + '/result_newnew_predict_validation_' + feature_name + '.txt', sep='\t', index=False)
-    result_auc.to_csv(save_path + '/' + str(method.split('_cv')[0]) + '/result_newnew_AUC_' + feature_name + '.txt', sep='\t',index=False)
+            acc_train, auc_train, f1_weighted_train, f1_macro_train = get_roc(y_train,predict_train[:, 1])
+            acc_test, auc_test, f1_weighted_test, f1_macro_test = get_roc(y_test,predict_test[:,1])
+            result_predict_train['num_' + str(i)] = predict_train[:, 1]
+            result_predict_test['num_' + str(i)] = predict_test[:, 1]
+            result_auc.loc[i, 'acc_train'] = acc_train
+            result_auc.loc[i, 'auc_train'] = auc_train
+            result_auc.loc[i, 'f1_weighted_train'] = f1_weighted_train
+            result_auc.loc[i, 'f1_macro_train'] = f1_macro_train
+            result_auc.loc[i, 'acc_test'] = acc_test
+            result_auc.loc[i, 'auc_test'] = auc_test
+            result_auc.loc[i, 'f1_weighted_test'] = f1_weighted_test
+            result_auc.loc[i, 'f1_macro_test'] = f1_macro_test
+
+    result_predict_train.to_csv(save_path + str(dataset) + '/' + str(method.split('_cv')[0]) + '/result_newnew_predict_train_' + feature_name + '.txt', sep='\t', index=False)
+    result_predict_test.to_csv(save_path + str(dataset) + '/' + str(method.split('_cv')[0]) + '/result_newnew_predict_test_' + feature_name + '.txt', sep='\t', index=False)
+    result_auc.to_csv(save_path + str(dataset) + '/' + str(method.split('_cv')[0]) + '/result_newnew_AUC_' + feature_name + '.txt',sep='\t', index=False)
 
 
 def main(args):
