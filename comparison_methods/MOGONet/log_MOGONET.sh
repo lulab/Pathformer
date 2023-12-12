c5931a61d8335d1c10ad19fbc7f8b0defd9600db
xfliu1995 <774327013@qq.com>
2023-12-12 21:10:34 +0800

    Update log_MOGONET.sh

--- comparison_methods/MOGONet/log_MOGONET.sh
+++ comparison_methods/MOGONet/log_MOGONET.sh
@@ -5,7 +5,7 @@ mkdir ${root_path}/comparison_methods/Result/BRCA_stage/MOGOnet
 mkdir ${root_path}/comparison_methods/Result/BRCA_stage/MOGOnet/merge
 cd ${root_path}/comparison_methods/MOGONet
 python3 ${root_path}/comparison_methods/MOGONet/main.py \
---data_path ${root_path}/data_TCGA/4.data_feature_filter_of_comparison_methods/2.data_feature_stage/ \
+--data_path ${root_path}/data_TCGA/4.data_feature_filter_of_comparison_methods/2.data_feature_stage/1000/ \
 --label_path ${root_path}/data_TCGA/2.sample_id/sample_cross_stage.txt \
 --feature_num 1000 \
 --dataset 1 \
@@ -19,7 +19,7 @@ mkdir ${root_path}/comparison_methods/Result/BRCA_survival/MOGOnet
 mkdir ${root_path}/comparison_methods/Result/BRCA_survival/MOGOnet/merge
 cd ${root_path}/comparison_methods/MOGONet
 python3 ${root_path}/comparison_methods/MOGONet/main.py \
---data_path ${root_path}/data_TCGA/4.data_feature_filter_of_comparison_methods/2.data_feature_survival/ \
+--data_path ${root_path}/data_TCGA/4.data_feature_filter_of_comparison_methods/2.data_feature_survival/1000/ \
 --label_path ${root_path}/data_TCGA/2.sample_id/sample_cross_survival.txt \
 --feature_num 1000 \
 --dataset 1 \
