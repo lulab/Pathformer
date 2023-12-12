72db13a4cf5b55f394b3d28b4414295d13565feb
xfliu1995 <774327013@qq.com>
2023-12-12 17:08:13 +0800

    Update log_MOGONET.sh

--- comparison_methods/MOGONet/log_MOGONET.sh
+++ comparison_methods/MOGONet/log_MOGONET.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 root_path=$"/apps/home/lulab_liuxiaofan/qhsky/project/multi_omics_paper/"
-mkdir ${root_path}/comparison_methods/Result/BRCA_stage/5.MOGOnet
-mkdir ${root_path}/comparison_methods/Result/BRCA_stage/5.MOGOnet/merge
+mkdir ${root_path}/comparison_methods/Result/BRCA_stage/MOGOnet
+mkdir ${root_path}/comparison_methods/Result/BRCA_stage/MOGOnet/merge
 cd ${root_path}/comparison_methods/MOGONet
 python3 ${root_path}/comparison_methods/MOGONet/main.py \
 --data_path ${root_path}/data_TCGA/4.data_feature_filter_of_comparison_methods/2.data_feature_stage/ \
@@ -12,11 +12,11 @@ python3 ${root_path}/comparison_methods/MOGONet/main.py \
 --data_type merge \
 --stop 200 \
 --num 2000 \
---save_path ${root_path}/comparison_methods/Result/BRCA_stage/5.MOGOnet/merge
+--save_path ${root_path}/comparison_methods/Result/BRCA_stage/MOGOnet/merge
 
 
-mkdir ${root_path}/comparison_methods/Result/BRCA_survival/5.MOGOnet
-mkdir ${root_path}/comparison_methods/Result/BRCA_survival/5.MOGOnet/merge
+mkdir ${root_path}/comparison_methods/Result/BRCA_survival/MOGOnet
+mkdir ${root_path}/comparison_methods/Result/BRCA_survival/MOGOnet/merge
 cd ${root_path}/comparison_methods/MOGONet
 python3 ${root_path}/comparison_methods/MOGONet/main.py \
 --data_path ${root_path}/data_TCGA/4.data_feature_filter_of_comparison_methods/2.data_feature_survival/ \
@@ -26,4 +26,5 @@ python3 ${root_path}/comparison_methods/MOGONet/main.py \
 --data_type merge \
 --stop 200 \
 --num 2000 \
---save_path ${root_path}/comparison_methods/Result/BRCA_survival/5.MOGOnet/merge
+--save_path ${root_path}/comparison_methods/Result/BRCA_survival/MOGOnet/merge
+
