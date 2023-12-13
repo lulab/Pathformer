9c02f7941ede8add5646744328dfa5c1cc369499
xfliu1995 <774327013@qq.com>
2023-12-14 00:41:29 +0800

    Update and rename log_BRCA_subtype.sh to log_BRCA_survival.sh

--- Pathformer_Interpretability/log_BRCA_survival.sh
+++ Pathformer_Interpretability/log_BRCA_survival.sh
@@ -5,16 +5,16 @@ root_path=$"/apps/home/lulab_liuxiaofan/qhsky/project/multi_omics_paper/"
 cd ${root_path}/Pathformer_code
 echo 'attention_map'
 python3 ${root_path}/code/Interpretability/1.attention_map.py \
---modal_all_path ${root_path}/Result/BRCA_subtype/modal_type_all.txt \
---modal_select_path ${root_path}/Result/BRCA_subtype/modal_type_all.txt \
+--modal_all_path ${root_path}/Result/BRCA_survival/modal_type_all.txt \
+--modal_select_path ${root_path}/Result/BRCA_survival/modal_type_all.txt \
 --gene_all ${root_path}/reference/gene_all.txt \
 --gene_select ${root_path}/reference/Pathformer_select_gene.txt \
 --pathway_gene_w ${root_path}/reference/Pathformer_pathway_gene_weight.npy \
 --pathway_crosstalk_network  ${root_path}/reference/Pathformer_pathway_crosstalk_network_matrix.npy \
---model_path ${root_path}/Result/BRCA_subtype/ckpt/BRCA_subtype_best.pth \
---data_path ${root_path}/data_TCGA/3.data_gene_embedding/merge/merge_data_subtype/data_all.npy \
---label_path ${root_path}/data_TCGA/2.sample_id/sample_cross_subtype_new_final.txt \
---save_path ${root_path}/Result/BRCA_subtype/Interpretability/ \
+--model_path ${root_path}/Result/BRCA_survival/ckpt/BRCA_survival_best.pth \
+--data_path ${root_path}/data_TCGA/3.data_gene_embedding/merge/merge_data_survival/data_all.npy \
+--label_path ${root_path}/data_TCGA/2.sample_id/sample_cross_survival_new_final.txt \
+--save_path ${root_path}/Result/BRCA_survival/Interpretability/ \
 --dataset 1 \
 --depth 3 \
 --heads  8 \
@@ -26,21 +26,21 @@ python3 ${root_path}/code/Interpretability/1.attention_map.py \
 
 echo 'attention_weight'
 python3 ${root_path}/code/Interpretability/1.attention_weight.py \
---modal_all_path ${root_path}/Result/BRCA_subtype/modal_type_all.txt \
---save_path ${root_path}/Result/BRCA_subtype/Interpretability/
+--modal_all_path ${root_path}/Result/BRCA_survival/modal_type_all.txt \
+--save_path ${root_path}/Result/BRCA_survival/Interpretability/
 
 echo 'SHAP_gene_sample'
 python3 ${root_path}/code/Interpretability/2.SHAP_gene_sample.py \
---modal_all_path ${root_path}/Result/BRCA_subtype/modal_type_all.txt \
---modal_select_path ${root_path}/Result/BRCA_subtype/modal_type_all.txt \
+--modal_all_path ${root_path}/Result/BRCA_survival/modal_type_all.txt \
+--modal_select_path ${root_path}/Result/BRCA_survival/modal_type_all.txt \
 --gene_all ${root_path}/reference/gene_all.txt \
 --gene_select ${root_path}/reference/Pathformer_select_gene.txt \
 --pathway_gene_w ${root_path}/reference/Pathformer_pathway_gene_weight.npy \
 --pathway_crosstalk_network  ${root_path}/reference/Pathformer_pathway_crosstalk_network_matrix.npy \
---model_path ${root_path}/Result/BRCA_subtype/ckpt/BRCA_subtype_best.pth \
---data_path ${root_path}/data_TCGA/3.data_gene_embedding/merge/merge_data_subtype/data_all.npy \
---label_path ${root_path}/data_TCGA/2.sample_id/sample_cross_subtype_new_final.txt \
---save_path ${root_path}/Result/BRCA_subtype/Interpretability/ \
+--model_path ${root_path}/Result/BRCA_survival/ckpt/BRCA_survival_best.pth \
+--data_path ${root_path}/data_TCGA/3.data_gene_embedding/merge/merge_data_survival/data_all.npy \
+--label_path ${root_path}/data_TCGA/2.sample_id/sample_cross_survival_new_final.txt \
+--save_path ${root_path}/Result/BRCA_survival/Interpretability/ \
 --dataset 1 \
 --depth 3 \
 --heads  8 \
@@ -52,16 +52,16 @@ python3 ${root_path}/code/Interpretability/2.SHAP_gene_sample.py \
 
 echo 'SHAP_pathway_sample'
 python3 ${root_path}/code/Interpretability/2.SHAP_pathway_sample.py \
---modal_all_path ${root_path}/Result/BRCA_subtype/modal_type_all.txt \
---modal_select_path ${root_path}/Result/BRCA_subtype/modal_type_all.txt \
+--modal_all_path ${root_path}/Result/BRCA_survival/modal_type_all.txt \
+--modal_select_path ${root_path}/Result/BRCA_survival/modal_type_all.txt \
 --gene_all ${root_path}/reference/gene_all.txt \
 --gene_select ${root_path}/reference/Pathformer_select_gene.txt \
 --pathway_gene_w ${root_path}/reference/Pathformer_pathway_gene_weight.npy \
 --pathway_crosstalk_network  ${root_path}/reference/Pathformer_pathway_crosstalk_network_matrix.npy \
---model_path ${root_path}/Result/BRCA_subtype/ckpt/BRCA_subtype_best.pth \
---data_path ${root_path}/data_TCGA/3.data_gene_embedding/merge/merge_data_subtype/data_all.npy \
---label_path ${root_path}/data_TCGA/2.sample_id/sample_cross_subtype_new_final.txt \
---save_path ${root_path}/Result/BRCA_subtype/Interpretability/ \
+--model_path ${root_path}/Result/BRCA_survival/ckpt/BRCA_survival_best.pth \
+--data_path ${root_path}/data_TCGA/3.data_gene_embedding/merge/merge_data_survival/data_all.npy \
+--label_path ${root_path}/data_TCGA/2.sample_id/sample_cross_survival_new_final.txt \
+--save_path ${root_path}/Result/BRCA_survival/Interpretability/ \
 --dataset 1 \
 --depth 3 \
 --heads  8 \
@@ -73,22 +73,22 @@ python3 ${root_path}/code/Interpretability/2.SHAP_pathway_sample.py \
 
 echo 'SHAP_pathway_modal'
 python3 ${root_path}/code/Interpretability/2.SHAP_pathway_modal.py \
---modal_all_path ${root_path}/Result/BRCA_subtype/modal_type_all.txt \
---modal_select_path ${root_path}/Result/BRCA_subtype/modal_type_all.txt \
+--modal_all_path ${root_path}/Result/BRCA_survival/modal_type_all.txt \
+--modal_select_path ${root_path}/Result/BRCA_survival/modal_type_all.txt \
 --pathway_name_path ${root_path}/reference/Pathformer_pathway.txt \
---save_path ${root_path}/Result/BRCA_subtype/Interpretability/
+--save_path ${root_path}/Result/BRCA_survival/Interpretability/
 
 echo 'SHAP_gene_modal'
 python3 ${root_path}/code/Interpretability/2.SHAP_gene_modal.py \
---modal_all_path ${root_path}/Result/BRCA_subtype/modal_type_all.txt \
---modal_select_path ${root_path}/Result/BRCA_subtype/modal_type_all.txt \
+--modal_all_path ${root_path}/Result/BRCA_survival/modal_type_all.txt \
+--modal_select_path ${root_path}/Result/BRCA_survival/modal_type_all.txt \
 --pathway_name_path ${root_path}/reference/Pathformer_pathway.txt \
 --gene_name_path ${root_path}/reference/Pathformer_select_gene.txt \
---save_path ${root_path}/Result/BRCA_subtype/Interpretability/
+--save_path ${root_path}/Result/BRCA_survival/Interpretability/
 
 echo 'pathway_sub_network_score'
 python3 ${root_path}/code/Interpretability/3.pathway_sub_network_score.py \
 --pathway_name_path ${root_path}/reference/Pathformer_pathway.txt \
 --pathway_crosstalk_network  ${root_path}/reference/Pathformer_pathway_crosstalk_network_matrix.npy \
---save_path ${root_path}/Result/BRCA_subtype/Interpretability/ \
+--save_path ${root_path}/Result/BRCA_survival/Interpretability/ \
 --cut_off 0.997
